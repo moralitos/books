5751ec2b66763fbb5f5f1e5c61f2b97964baefdc
moralesalberto <alberto@moralitos.com>
2012-12-31 17:05:30 -0500

    blank rails app with sqlite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Books::Application.config.secret_token = '706cbb9b4149301d90a93acbf7255586054158339d67251b692d3ef98d76c1be8f8c883b7ad37a86502056ecd257765aec18028476391026027e982b132e16d0'
